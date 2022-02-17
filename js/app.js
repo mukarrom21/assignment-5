c0e48cc98576ba628b1d067848fd6502a216a47c
mukarrom21 <mukarrom21@gmail.com>
2022-02-17 20:16:14 +0600

    error handling

--- js/app.js
+++ js/app.js
@@ -10,48 +10,56 @@ const savingAmount = document.getElementById("saving-amount");
 const remainingBalance = document.getElementById("remaining-balance");
 
 function calcMoney(myId) {
-  if (myId == true) {
-    totalExpenses.innerText =
-      parseInt(foodInput.value) +
-      parseInt(rentInput.value) +
-      parseInt(clothesInput.value);
-    balance.innerText =
-      parseFloat(incomeInput.value) - parseFloat(totalExpenses.innerText);
-  } else {
-    savingAmount.innerText =
-      (parseFloat(incomeInput.value) * parseFloat(saveInput.value)) / 100;
-    remainingBalance.innerText =
-      parseFloat(balance.innerText) - parseFloat(savingAmount.innerText);
+  const inpt = document.getElementsByTagName("input");
+  for (let input of inpt) {
+    if (isNaN(input.value) || input.value < 0 || input.value == "") {
+      input.value = 0;
+      document.getElementById("err").style.display = "block";
+    } else {
+      alert(balance.innerText);
+      // document.getElementById("err").style.display = "none";
+
+      if (myId == true) {
+        totalExpenses.innerText =
+          parseInt(foodInput.value) +
+          parseInt(rentInput.value) +
+          parseInt(clothesInput.value);
+        balance.innerText =
+          parseFloat(incomeInput.value) - parseFloat(totalExpenses.innerText);
+      } else {
+        savingAmount.innerText =
+          (parseFloat(incomeInput.value) * parseFloat(saveInput.value)) / 100;
+        remainingBalance.innerText =
+          parseFloat(balance.innerText) - parseFloat(savingAmount.innerText);
+      }
+    }
   }
 }
-// sum total expenses
+
 // Calculate button
 document.getElementById("calculate-btn").addEventListener("click", function () {
   calcMoney(true);
-  // totalExpenses.innerText = parseInt(foodInput.value) + parseInt(rentInput.value) + parseInt(clothesInput.value);
-  // const totalExpenses =parseInt(foodInput.value) + parseInt(rentInput.value) + parseInt(clothesInput.value);
-  // const balanceTotal =
-  //   parseFloat(incomeInput.value) - parseFloat(totalExpenses);
-
-  // balance.innerText = parseFloat(incomeInput.value) - parseFloat(totalExpenses.innerText);
 });
 // Save button
 document.getElementById("seve-btn").addEventListener("click", function () {
   calcMoney(false);
-  // const saveAmount =
-  //   (parseFloat(incomeInput.value) * parseFloat(saveInput.value)) / 100;
-  // savingAmount.innerText = saveAmount;
-  // remainingBalance.innerText =
-  //   parseFloat(balance.innerText) - parseFloat(saveAmount);
 });
 
-// incomeInput.addEventListener("keyup", function () {
-//   balance.innerText =
-//     parseFloat(incomeInput.value) - parseFloat(totalExpensesInput.innerText);
-// });
-// foodInput.addEventListener("keyup", function () {
-//   totalExpensesInput.innerText =
-//     parseFloat(foodInput.value) +
-//     parseFloat(rentInput.value) +
-//     parseFloat(clothesInput.value);
-// });
+
+// akaj
+document.getElementById("err-btn").addEventListener("click", function () {
+  const inpt = document.getElementsByTagName("input");
+  for (let input of inpt) {
+    if (isNaN(input.value) || input.value < 0 || input.value == "") {
+      document.getElementById("err").style.display = "block";
+      input.value = 0;
+    } else {
+    }
+  }
+});
+
+const inpt = document.getElementsByTagName("input");
+for (let input of inpt) {
+  console.log(input);
+  input.addEventListener("keyup", function () {});
+}
